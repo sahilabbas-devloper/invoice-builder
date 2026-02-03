ca9e905922932d5d2ad93230cbd26a93c4452d18
Sahil Abbas <sa9300432@gmail.com>
2026-02-03 14:12:24 +0530

    refresh error fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div>
-<section className='w-full h-14 p-4 flex gap-20 md:gap-150  bg-white shadow-lg fixed items-center'>
+<section className='w-full h-14 p-4 flex gap-15 md:gap-150  bg-white shadow-lg fixed items-center'>
 <div>
   <div className='flex items-center'>
-    <img className='md:w-full md:max-w-10 w-full max-w-8   h-auto object-cover md:ml-5  mix-blend-darken' src="https://cdn-icons-png.flaticon.com/512/6953/6953015.png" alt="" />
+    <img className='md:w-full md:max-w-10 w-full max-w-6   h-auto object-cover md:ml-5  mix-blend-darken' src="https://cdn-icons-png.flaticon.com/512/6953/6953015.png" alt="" />
     <h1 className=' text-md md:text-xl font-semibold'>InvoiceGen</h1>
   </div>
 </div>
@@ -17,7 +17,7 @@ function Header() {
 
 
 
-<nav className='md:text-md   text-sm font-semibold text-gray-700 flex gap-4 md:gap-15'>
+<nav className='md:text-md   text-sm font-semibold text-gray-700 flex gap-3 md:gap-15'>
 <Link to="/Home" className=' hover:text-green-600'>Home</Link>
 <Link to="/Purchase" className=' hover:text-green-600'>Purchase</Link>
 <Link to="/View" className=' hover:text-green-600'>View</Link>
